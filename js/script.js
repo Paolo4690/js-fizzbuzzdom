ca8da0a7a1acf3509c2713bb501c2d2d69b7eb3c
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-02-23 14:32:14 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const areaSquare = document.querySelector(".cont-square");
+
+for (let i = 1; i <= 100; i++) {
+    let numero = i;
+    let multipli3 = i % 3 === 0;
+    let multipli5 = i % 5 === 0;
+    let bG = 'blue'
+    
+    if (multipli3) {
+        bG = 'green';
+        numero = 'Fizz';
+    };  
+
+    if (multipli5) {
+        bG = 'yellow';
+        numero = 'Buzz';
+    };
+
+    if (multipli3 && multipli5) {
+        bG = 'red';
+        numero = 'FizzBuzz';
+    };
+
+    areaSquare.innerHTML += `
+        <div class="box ${bG}">
+            ${numero}
+        </div>`;
+};
